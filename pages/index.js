bc0b9387147826709f32b894d5864cacdbd1c93e
Jason Brown <browniefed@gmail.com>
2017-08-12 10:09:34 -0700

    Add stuff

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,42 @@
-import React, { Component } from 'react';
-import { rehydrate, css } from 'glamor'
-import glamorous from 'glamorous'
+import React, { Component } from "react";
+import { rehydrate, css } from "glamor";
+import glamorous from "glamorous";
 
+import { registerNewsLetter } from "../api";
 
 class ReactCoach extends Component {
+  state = {
+    email: "",
+    thanks: false,
+  };
+  handleNewsletter = async (e) => {
+    e.preventDefault();
+
+    await registerNewsLetter(null, this.state.email, "reactcoach.com", "React");
+    this.setState({
+      email: "",
+      thanks: true,
+    });
+  };
   render() {
     return (
       <div>
         <h2>Welcome to React Coach</h2>
         <p>
-          We'll teach all you need to know about React and the ecosystem through tutorials, screencasts, and interactive demos.
+          We'll teach all you need to know about React and the ecosystem through tutorials,
+          screencasts, and interactive demos.
         </p>
+        <div>
+          <form onSubmit={this.handleNewsletter}>
+            <input
+              type="email"
+              required
+              value={this.state.email}
+              onChange={e => this.setState({ email: e.target.value })}
+            />
+            <button type="submit">Sign Up</button>
+          </form>
+        </div>
       </div>
     );
   }
