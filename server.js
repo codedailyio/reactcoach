5359a90900f94d36ed1019cf5df58ad99dec715f
Jason Brown <browniefed@gmail.com>
2017-08-18 20:56:39 -0700

    REmove stupid logging

--- server.js
+++ server.js
@@ -8,40 +8,22 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const server = new Hapi.Server();
 
-// add request logging (optional)
-const pluginOptions = [
-  {
-    register: Good,
-    options: {
-      reporters: {
-        console: [
-          {
-            module: "good-console",
-          },
-          "stdout",
-        ],
-      },
-    },
-  },
-];
 
 app.prepare().then(() => {
   server.connection({ port });
-  server.register(pluginOptions).then(() => {
-    server.route({
-      method: "GET",
-      path: "/{p*}" /* catch all route */,
-      handler: defaultHandlerWrapper(app),
-    });
-
-    server
-      .start()
-      .catch(error => {
-        console.log("Error starting server");
-        console.log(error);
-      })
-      .then(() => {
-        console.log(`> Ready on http://localhost:${port}`);
-      });
+  server.route({
+    method: "GET",
+    path: "/{p*}" /* catch all route */,
+    handler: defaultHandlerWrapper(app),
   });
+
+  server
+    .start()
+    .catch(error => {
+      console.log("Error starting server");
+      console.log(error);
+    })
+    .then(() => {
+      console.log(`> Ready on http://localhost:${port}`);
+    });
 });
